5514c3cabf13bdce682f9b4fe6957fc592850544
Omar Torres <omartorres980.com>
2023-08-01 12:46:04 -0700

    added nav bar spacing, board and join pages

--- src/about/Board.js
+++ src/about/Board.js
@@ -1,8 +1,114 @@
 import "./Board.css"
+import placehold from "../images/ckilogo.png"
 
 function Board() {
     return (
-      <p className = "Board"> Board!</p>
+      <div className="board-members">
+        <h1>2023-2024 UCSD CKI Board</h1>
+        <h2>Executive Board</h2>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>1st year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>2nd year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>3rd year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>4th year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>5th year</p>
+          </div>
+        </div>
+
+        <h2>Appointed Board</h2>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>1st year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>2nd year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>3rd year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>4th year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>5th year</p>
+          </div>
+        </div>
+        <div className="card">
+          <img src={placehold} alt="guy" />
+          <div>
+            <h3>Chris Pratt</h3>
+            <p>President</p>
+            <p>He rules!</p>
+            <p>6th year</p>
+          </div>
+        </div>
+
+      </div>
     );
   }
   

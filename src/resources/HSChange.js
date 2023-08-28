397ca2691d1ad1fc40bcde411d72e6ccdc75af98
Alex <alex@gaofamily.org>
2023-08-28 13:11:50 -0700

    happy/sad change

--- src/resources/HSChange.js
+++ src/resources/HSChange.js
@@ -1,9 +1,15 @@
 import "./HSChange.css"
+import qrcode from "../images/misc/hsqrcode.jpeg"
 
 function HSChange() {
     return (
-      <div>
-        <p className = "HSChange"> HSChange!</p>
+      <div class = "items">
+        <h1 className = "HSChange"> Happy Sad Change!</h1>
+        <img src = {qrcode}></img>
+        <div class = "paragraphs">
+          <p class = "about">We support the organization Pediatric Trauma Program (PTP)!</p>
+          <p class = "venmo">Venmo: @UcsdCircleK</p>
+        </div>
       </div>
     );
   }

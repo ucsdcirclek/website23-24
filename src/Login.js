a755ae01a2ab3c200496a905d2a876015f30ec1c
Alex <alex@gaofamily.org>
2023-10-06 12:25:18 -0700

    added basic google login

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,24 @@
 import "./Login.css"
+import { GoogleLogin } from '@react-oauth/google';
+
 function Login() {
-    return <h1 className = "login"> Work in Progress..</h1>;
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+  const errorMessage = (error) => {
+    console.log(error);
+  };
+    return  (
+    <div>
+      <h1 className = "login"> 
+         Work in Progress..
+       </h1>
+       <h2>React Google Login</h2>
+            <br />
+            <br />
+        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+    </div>);
+    
   }
   
   export default  Login;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="178149121589-v3fkto5htqctroh74oie081vg1nae8ne.apps.googleusercontent.com">
+    <React.StrictMode>
+      <BrowserRouter>
+       <App/>
+      </BrowserRouter>,
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

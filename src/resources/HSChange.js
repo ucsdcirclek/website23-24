73a2caa436b72497fc612d1b4ff2307dc5b640d7
Alex <alex@gaofamily.org>
2024-01-15 10:04:45 -0800

    random change

--- src/resources/HSChange.js
+++ src/resources/HSChange.js
@@ -4,7 +4,7 @@ import qrcode from "../images/misc/hsqrcode.jpeg"
 function HSChange() {
     return (
       <div class = "items">
-        <h1 className = "HSChange"> Happy Sad Change!</h1>
+        <h1 className = "HSChange"> Happy Sad Change.</h1>
         <img src = {qrcode} alt="HSChange"></img>
         <p class = "about">All proceeds go towards the <a href="https://www.cnhfoundation.org/how-to-help/h2h-pediatric-trauma-program/">Pediatric Trauma Program (PTP)!</a></p>
       </div>
